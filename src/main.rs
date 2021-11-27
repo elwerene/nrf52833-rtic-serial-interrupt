10753df06f7bcb25d6d03d42cd8ed4c38b88499e
René Rössler <rene@freshx.de>
2021-11-27 22:14:34 +0100

    works!

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,9 @@ fn panic() -> ! {
 const APP: () = {
     struct Resources {
         serial0: UarteRx<UARTE0>,
+        uarte0: pac::UARTE0,
         serial1: UarteRx<UARTE1>,
+        uarte1: pac::UARTE1,
     }
 
     #[init]
@@ -31,73 +33,73 @@ const APP: () = {
         let p = cx.device;
         let p0parts = p0::Parts::new(p.P0);
 
-        let serial0 = {
-            let pins = uarte::Pins {
+        let (uarte0, _) = uarte::Uarte::new(
+            p.UARTE0,
+            uarte::Pins {
                 txd: p0parts.p0_21.into_push_pull_output(Level::High).degrade(),
                 rxd: p0parts.p0_02.into_floating_input().degrade(),
                 cts: None,
                 rts: None,
-            };
+            },
+            uarte::Parity::EXCLUDED,
+            uarte::Baudrate::BAUD115200,
+        )
+        .free();
 
-            p.UARTE0.intenset.modify(|_, w| w.rxdrdy().set_bit());
+        uarte0.intenset.modify(|_, w| w.rxdrdy().set_bit());
 
-            let serial = uarte::Uarte::new(
-                p.UARTE0,
-                pins,
-                uarte::Parity::EXCLUDED,
-                uarte::Baudrate::BAUD115200,
-            );
+        static mut SERIAL0_BUF: [u8; 1] = [0; 1];
+        let mut serial0 = UarteRx::new(unsafe { &mut SERIAL0_BUF }).expect("Could not create rx");
+        serial0.read().ok();
 
-            static mut TX_BUF: [u8; 1] = [0; 1];
-            static mut RX_BUF: [u8; 1] = [0; 1];
-
-            let (_, rx) = serial
-                .split(unsafe { &mut TX_BUF }, unsafe { &mut RX_BUF })
-                .expect("Could not split serial0 to rx/tx");
-            rx
-        };
-        let serial1 = {
-            let pins = uarte::Pins {
+        let (uarte1, _) = uarte::Uarte::new(
+            p.UARTE1,
+            uarte::Pins {
                 txd: p0parts.p0_22.into_push_pull_output(Level::High).degrade(),
                 rxd: p0parts.p0_03.into_floating_input().degrade(),
                 cts: None,
                 rts: None,
-            };
+            },
+            uarte::Parity::EXCLUDED,
+            uarte::Baudrate::BAUD115200,
+        )
+        .free();
 
-            p.UARTE1.intenset.modify(|_, w| w.rxdrdy().set_bit());
+        uarte1.intenset.modify(|_, w| w.rxdrdy().set_bit());
 
-            let serial = uarte::Uarte::new(
-                p.UARTE1,
-                pins,
-                uarte::Parity::EXCLUDED,
-                uarte::Baudrate::BAUD115200,
-            );
+        static mut SERIAL1_BUF: [u8; 1] = [0; 1];
+        let mut serial1 = UarteRx::new(unsafe { &mut SERIAL1_BUF }).expect("Could not create rx");
+        serial1.read().ok();
 
-            static mut TX_BUF: [u8; 1] = [0; 1];
-            static mut RX_BUF: [u8; 1] = [0; 1];
-
-            let (_, rx) = serial
-                .split(unsafe { &mut TX_BUF }, unsafe { &mut RX_BUF })
-                .expect("Could not split serial1 to rx/tx");
-            rx
-        };
-
-        init::LateResources { serial0, serial1 }
+        init::LateResources {
+            serial0,
+            uarte0,
+            serial1,
+            uarte1,
+        }
     }
 
-    #[task(binds = UARTE0_UART0, resources = [serial0])]
+    #[task(binds = UARTE0_UART0, resources = [serial0, uarte0])]
     fn uarte0_interrupt(cx: uarte0_interrupt::Context) {
         defmt::println!("uarte0 interrupt");
-        if let Ok(b) = cx.resources.serial0.read() {
+        while let Ok(b) = cx.resources.serial0.read() {
             defmt::println!("Byte on serial0: {}", b)
         }
+        cx.resources
+            .uarte0
+            .events_rxdrdy
+            .write(|w| unsafe { w.bits(0) });
     }
 
-    #[task(binds = UARTE1, resources = [serial1])]
+    #[task(binds = UARTE1, resources = [serial1, uarte1])]
     fn uarte1_interrupt(cx: uarte1_interrupt::Context) {
         defmt::println!("uarte1 interrupt");
-        if let Ok(b) = cx.resources.serial1.read() {
+        while let Ok(b) = cx.resources.serial1.read() {
             defmt::println!("Byte on serial1: {}", b)
         }
+        cx.resources
+            .uarte1
+            .events_rxdrdy
+            .write(|w| unsafe { w.bits(0) });
     }
 };
